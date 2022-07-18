d4f6f51f478166ef25bbfd5419a9ec9b06c58a2b
huangshihao <hsh@atguigu.com>
2022-07-18 18:23:43 +0800

    Second commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class Test {
     public static void main(String[] args) {
         System.out.println("黄世豪的java学有小成。");
+        System.out.println("继续学，才发现鸡毛个有小成啊。");
     }
 }
