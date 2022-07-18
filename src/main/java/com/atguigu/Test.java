ed67e671e2a07d9134566df897d325be5c7faaaf
huangshihao <hsh@atguigu.com>
2022-07-18 23:41:39 +0800

    Third commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("黄世豪的java学有小成。");
         System.out.println("继续学，才发现鸡毛个有小成啊。");
         System.out.println("越学越感觉，我是一个井底之蛙。");
+        System.out.println("虽是井底之蛙，也得向大世界进发");
     }
 }
